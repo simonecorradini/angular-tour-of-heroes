eb7c0aaae0727287970b232aa2cd8b8e9eccf287
simoneCorradini <simone.corradini@gmail.com>
2020-10-29 13:00:52 +0100

    fine sezione 6 corso

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Corradini' },
+  { id: 12, name: 'Priore' },
+  { id: 13, name: 'Di Raimo' },
+  { id: 14, name: 'Cicino' },
+  { id: 15, name: 'Del Toso' },
+  { id: 16, name: 'Zabatta' },
+  { id: 17, name: 'Pomponi' },
+  { id: 18, name: 'Stevenazzi' },
+  { id: 19, name: 'Castiglia' },
+  { id: 20, name: 'Bove' }
+];
